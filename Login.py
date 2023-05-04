81809f40b9cb1a164465f05f86baf5c394d41995
Itallo-Gabriel-Melo <itallogabriel2003@gmail.com>
2023-05-04 13:47:03 -0300

    adicionando sites

--- Login.py
+++ Login.py
@@ -1,4 +1,16 @@
+from random import shufle
 login = input('Login: ')
 senha = input('Senha: ')
 print('Bem vindo {}'.format(login))
-print('Nova atualização')
+print('site atualizado')
+print('venha fazer suas compras')
+p1 = 'maçã'
+p2 = 'laranja'
+p3 = 'abacaxi'
+p4 = 'melancia'
+produtos = [p1,p2,p3,p4]
+print('Esta é a ordem dos produtos')
+print(produtos)
+print('Porem alguem andou baguncando e agora esta assim')
+shufle(produtos)
+print(produtos)
